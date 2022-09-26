2672171cb83d1608b537835fd82a34f6fb4e0473
ACLzz <daniil@wizer-training.com>
2022-09-27 00:30:52 +0300

    add download button on tags panel

--- app/src/main/java/com/example/r34university/DetailActivity.kt
+++ app/src/main/java/com/example/r34university/DetailActivity.kt
@@ -1,16 +1,23 @@
 package com.example.r34university
 
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.core.view.GestureDetectorCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -20,7 +27,11 @@ import com.google.android.flexbox.FlexDirection
 import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayoutManager
 import parsers.ContentParser
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 import kotlin.math.abs
+
 
 const val SWIPE_THRESHOLD = 120
 const val SWIPE_VELOCITY_THRESHOLD = 120
@@ -81,48 +92,10 @@ class DetailActivity: AppCompatActivity(), Communicator, GestureDetector.OnGestu
         binding.tagsLayout.setOnClickListener { hideTags() }
         binding.tagsPlaceholder.setOnClickListener { if (binding.tagsLayout.visibility == ViewGroup.VISIBLE) hideTags() else showTags() }
         binding.tagsViewToggler.setOnClickListener { showTags() }
+        binding.downloadButton.setOnClickListener { downloadImage() }
         mDetector = GestureDetectorCompat(this, this)
 
         showImage()
-    }
-
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        return if (mDetector.onTouchEvent(ev)) {
-            true
-        } else {
-            super.dispatchTouchEvent(ev)
-        }
-    }
-
-    override fun onLongPress(event: MotionEvent) {}
-    override fun onDown(event: MotionEvent): Boolean { return false }
-    override fun onShowPress(event: MotionEvent) {}
-    override fun onScroll(event1: MotionEvent, event2: MotionEvent, distanceX: Float, distanceY: Float): Boolean { return false }
-    override fun onSingleTapUp(event: MotionEvent): Boolean { return false }
-    override fun onFling(
-        e1: MotionEvent,
-        e2: MotionEvent,
-        velocityX: Float,
-        velocityY: Float
-    ): Boolean {
-        var result = false
-        try {
-            val diffY = e2.y - e1.y
-            val diffX = e2.x - e1.x
-            if (abs(diffX) > abs(diffY)) {
-                if (abs(diffX) > SWIPE_THRESHOLD && abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-                    if (diffX > 0) {
-                        goPrevImage()
-                    } else {
-                        goNextImage()
-                    }
-                    result = true
-                }
-            }
-        } catch (exception: Exception) {
-            exception.printStackTrace()
-        }
-        return result
     }
 
     private fun goNextImage() {
@@ -176,6 +149,44 @@ class DetailActivity: AppCompatActivity(), Communicator, GestureDetector.OnGestu
             .into(binding.fullImageView)
     }
 
+    private fun downloadImage() {
+        val image = binding.fullImageView.drawable.toBitmap()
+        val filename = currentImage.detail?.split("=")?.last()
+        val path = Environment.DIRECTORY_PICTURES + "/Rule34Universe"
+        var fos: OutputStream? = null
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            contentResolver?.also { resolver ->
+                val contentValues = ContentValues().apply {
+
+                    //putting file information in content values
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, path)
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                        put(MediaStore.MediaColumns.ALBUM, "Rule34Universe")
+                    }
+                }
+
+                val imageUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            val imagesDir =
+                Environment.getExternalStoragePublicDirectory(path)
+            val f = File(imagesDir, filename)
+            fos = FileOutputStream(f)
+        }
+
+        fos?.use {
+            //Finally writing the bitmap to the output stream that we opened
+            image.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            Toast.makeText(this, "Saved to Gallery", Toast.LENGTH_LONG).show()
+        }
+    }
+
     private fun searchTag(tagId: Int) {
         val tag = tagsList[tagId]
 
@@ -190,4 +201,42 @@ class DetailActivity: AppCompatActivity(), Communicator, GestureDetector.OnGestu
 
     override fun passSearchResults(results: List<ImageItem>) {}
     override fun passPagesCount(count: Int) {}
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        return if (mDetector.onTouchEvent(ev)) {
+            true
+        } else {
+            super.dispatchTouchEvent(ev)
+        }
+    }
+
+    override fun onLongPress(event: MotionEvent) {}
+    override fun onDown(event: MotionEvent): Boolean { return false }
+    override fun onShowPress(event: MotionEvent) {}
+    override fun onScroll(event1: MotionEvent, event2: MotionEvent, distanceX: Float, distanceY: Float): Boolean { return false }
+    override fun onSingleTapUp(event: MotionEvent): Boolean { return false }
+    override fun onFling(
+        e1: MotionEvent,
+        e2: MotionEvent,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        var result = false
+        try {
+            val diffY = e2.y - e1.y
+            val diffX = e2.x - e1.x
+            if (abs(diffX) > abs(diffY)) {
+                if (abs(diffX) > SWIPE_THRESHOLD && abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffX > 0) {
+                        goPrevImage()
+                    } else {
+                        goNextImage()
+                    }
+                    result = true
+                }
+            }
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+        }
+        return result
+    }
 }
